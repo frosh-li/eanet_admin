81757dd7c778825ecd5b170007893eab01315df0
李俊良 <lijunliang6@wanda.cn>
2015-05-08 10:08:14 +0800

    大量修改样式，与线上环境保持一致

--- js/controllers/authsController.js
+++ js/controllers/authsController.js
@@ -31,7 +31,49 @@ authsControllers.controller('authsCreateUser', ['$http','$scope','AuthGroupList'
 authsControllers.controller('authsApiList', ['$http','$scope','AuthApiList',
   function($http,$scope,AuthApiList) {
 
-    $scope.lists = AuthApiList.query();
+    // $scope.lists = AuthApiList.query();
+    $scope.hanldeTree = function(obj){
+        var env = window.event || e;
+        console.log(env.target);
+    }
+    $scope.lists = {
+        data:[
+          {
+            name:"交易管理",
+            innerApi:[
+              {
+                "name":"交易列表",
+                "url":"order/list"
+              },
+              {
+                "name":"新建交易",
+                "url":"order/list"
+              },
+              {
+                "name":"退换货",
+                "url":"order/list"
+              },
+            ]
+          },
+          {
+            name:"权限管理",
+            innerApi:[
+              {
+                "name":"新建权限组",
+                "url":"order/list"
+              },
+              {
+                "name":"新建用户",
+                "url":"order/list"
+              },
+              {
+                "name":"权限组列表",
+                "url":"order/list"
+              },
+            ]
+          }
+        ]
+    };
     console.log($scope.lists);
 
   }]);
