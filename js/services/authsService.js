7ba38e81c7a20492abad54113417679bcb74bd5e
李俊良 <lijunliang6@wanda.cn>
2015-05-14 10:17:16 +0800

    修改API地址

--- js/services/authsService.js
+++ js/services/authsService.js
@@ -18,7 +18,7 @@ authsServices.factory('AuthUser', ['$resource',
 
 authsServices.factory('AuthGroupList', ['$resource',
   function($resource){
-    return $resource(globalConfig.api + 'auths/groupList/:id', {id:'@id',search:'@search'}, {
+    return $resource(globalConfig.api + 'auths/group/:id', {id:'@id',search:'@search'}, {
       query: {method:'GET', params:{}, isArray:false},
       //get:{method:'GET', params: {ids: "@ids"}, isArray:false},//不用delete方法也不用这个了
       getOne:{method:'GET', params: {id: "@id"}, isArray:false},//为了优化请求地址，实际上应该是{id: "@id"}
