f0abb55eca9b7f233413d139f223d8dd62e85685
kanghongyan <khongyan379@sina.com>
2015-06-19 14:50:49 +0800

    update 积分交易明细查询

--- modules/point/js/controllers.js
+++ modules/point/js/controllers.js
@@ -1,182 +1,243 @@
-'use strict'
+'use strict';
 
 //账户总览
 storeApp.register.controller('account', ['$scope', 'accountService',
-    function($scope, accountService) {
-        
-        $scope.currentPage = 1;
-        $scope.totalPage = 1;
-        $scope.pageSize = 10;
-        $scope.pages = [];
-        $scope.endPage = 1;
+  function($scope, accountService) {
 
-        $scope.load = function() {
-            $scope.accountInfo = accountService.query({
-                    merchantId: storeApp.userInfo.merchantID,
-                    offset: $scope.currentPage,
-                    limit: $scope.pageSize
-                },
-                function() {
-                    // 数据返回成功
-                    $scope.totalCount = $scope.accountInfo.data.totalCount;
-                    $scope.totalPage = Math.ceil($scope.totalCount / $scope.pageSize);
-                    $scope.endPage = $scope.totalPage;
-                    //生成数字链接
-                    if ($scope.currentPage > 1 && $scope.currentPage < $scope.totalPage) {
-                        $scope.pages = [
-                            $scope.currentPage - 1,
-                            $scope.currentPage,
-                            $scope.currentPage + 1
-                        ];
-                    } else if ($scope.currentPage == 1 && $scope.totalPage > 1) {
-                        $scope.pages = [
-                            $scope.currentPage,
-                            $scope.currentPage + 1
-                        ];
-                    } else if ($scope.currentPage == $scope.totalPage && $scope.totalPage > 1) {
-                        $scope.pages = [
-                            $scope.currentPage - 1,
-                            $scope.currentPage
-                        ];
-                    }
-                    console.log($scope);
-                },
-                function() {
-                    // 数据返回失败
-                    console.log("response failed!");
-                }
-            );
-        };
+    $scope.currentPage = 1;
+    $scope.totalPage = 1;
+    $scope.pageSize = 10;
+    $scope.pages = [];
+    $scope.endPage = 1;
 
+    $scope.load = function() {
+      $scope.accountInfo = accountService.query({
+            merchantId: storeApp.userInfo.merchantID,
+            offset: $scope.currentPage,
+            limit: $scope.pageSize
+          },
+          function() {
+            // 数据返回成功
+            $scope.totalCount = $scope.accountInfo.data.totalCount;
+            $scope.totalPage = Math.ceil($scope.totalCount / $scope.pageSize);
+            $scope.endPage = $scope.totalPage;
+            //生成数字链接
+            if ($scope.currentPage > 1 && $scope.currentPage < $scope.totalPage) {
+              $scope.pages = [
+                $scope.currentPage - 1,
+                $scope.currentPage,
+                $scope.currentPage + 1
+              ];
+            } else if ($scope.currentPage == 1 && $scope.totalPage > 1) {
+              $scope.pages = [
+                $scope.currentPage,
+                $scope.currentPage + 1
+              ];
+            } else if ($scope.currentPage == $scope.totalPage && $scope.totalPage > 1) {
+              $scope.pages = [
+                $scope.currentPage - 1,
+                $scope.currentPage
+              ];
+            }
+            console.log($scope);
+          },
+          function() {
+            // 数据返回失败
+            console.log("response failed!");
+          }
+      );
+    };
+
+    $scope.load();
+
+    $scope.next = function() {
+      if ($scope.currentPage < $scope.totalPage) {
+        $scope.currentPage++;
         $scope.load();
+      }
+    };
 
-        $scope.next = function() {
-            if ($scope.currentPage < $scope.totalPage) {
-                $scope.currentPage++;
-                $scope.load();
-            }
-        };
+    $scope.prev = function() {
+      if ($scope.currentPage > 1) {
+        $scope.currentPage--;
+        $scope.load();
+      }
+    };
 
-        $scope.prev = function() {
-            if ($scope.currentPage > 1) {
-                $scope.currentPage--;
-                $scope.load();
-            }
-        };
+    $scope.loadPage = function(page) {
+      $scope.currentPage = page;
+      $scope.load();
+    };
 
-        $scope.loadPage = function(page) {
-            $scope.currentPage = page;
-            $scope.load();
-        };
-
-        $scope.changeLimit = function() {
-            $scope.currentPage = 1;
-            $scope.load();
-        };
-    }
+    $scope.changeLimit = function() {
+      $scope.currentPage = 1;
+      $scope.load();
+    };
+  }
 ]);
 
 //账户提现
 storeApp.register.controller('drawcash', ['$scope', 'drawcashService',
-    function($scope, drawcashService) {
-        // 积分
-        $scope.point = '';
-        $scope.withdraw = function() {
-            $scope.drawResult = drawcashService.save({
-                merchantId: storeApp.userInfo.merchantID,
-                point: $scope.point,
-                operaterId: storeApp.userInfo.uid
-            }, function() {
-                // 操作成功
+  function($scope, drawcashService) {
+    // 积分
+    $scope.point = '';
+    $scope.withdraw = function() {
+      $scope.drawResult = drawcashService.save({
+        merchantId: storeApp.userInfo.merchantID,
+        point: $scope.point,
+        operaterId: storeApp.userInfo.uid
+      }, function() {
+        // 操作成功
 
-            }, function() {});
-        }
+      }, function() {});
     }
+  }
 ]);
 
 //交易明细
-storeApp.register.controller('transactions', ['$scope',
-    function($scope) {
-      $('.datepicker').datepicker($.datepicker.regional[ "zh-TW" ]);
-      $scope.today=new Date();
-      $scope.earlyDay= function (dayCount) {
-        var day=new Date();
-        day.setDate(day.getDate()+dayCount);
-        return day;
-      }
+storeApp.register.controller('transactions', ['$scope', 'transactionService','$filter','calculateService',
+  function ($scope, transactionService,$filter,calculateService) {
+    //初始化日历
+    $('.datepicker').datepicker($.datepicker.regional["zh-TW"]);
 
+    //日期
+    $scope.today = new Date();
+    $scope.earlyDay = function (dayCount) {
+      var day = new Date();
+      day.setDate(day.getDate() + dayCount);
+      $scope.startTime=$filter("date")(day,"yyyy-MM-dd");
+    };
+    $scope.earlyMonth= function (monthCount) {
+      var day=new Date();
+      day.setMonth(day.getMonth() + monthCount);
+      if(monthCount===0){
+        day.setDate(1);
+      }
+      $scope.startTime=$filter("date")(day,"yyyy-MM-dd");
+    };
+    //input
+    $scope.earlyDay(-7);
+    $scope.endTime=$filter("date")($scope.today,"yyyy-MM-dd");
+
+
+    //查询
+    $scope.queryTransactionInfo= function () {
+      $scope.transactionInfo = transactionService.query(
+          {
+            startTime:$scope.startTime,
+            endTime:$scope.endTime
+          },
+          function () {
+            $scope.transactionData=$scope.transactionInfo.data;
+            console.log($scope.transactionData);
+            $scope.transactionData.result= {
+              pointAdd:$scope.transactionData.pointAdd||0.00,
+              pointReduce:$scope.transactionData.pointReduce||0.00,
+              recharge:$scope.transactionData.recharge||0.00,
+              withdraw:$scope.transactionData.withdraw||0.00,
+              pointBalance:function () {
+                return calculateService.sub(this.pointAdd,this.pointReduce);
+              },
+              pointTotal: function () {
+                var point1=this.pointBalance();
+                var point2=calculateService.sub(this.recharge,this.withdraw);
+                return calculateService.add(point1,point2)
+              }
+            }
+          });
+    };
+    $scope.queryTransactionInfo();
+
+
+
+
+    //点击查询
+    $scope.query = function () {
+      $scope.queryTransactionInfo();
     }
+
+  }
 ]);
 
 
 //积分月报
-storeApp.register.controller('monthlyreport', ['$scope', 'monthReportService',
-    function($scope, monthReportService) {
-        $scope.monthReportInfo = monthReportService.query({},function(){
-          $scope.monthReport=$scope.monthReportInfo.data.total;
+storeApp.register.controller('monthlyreport', ['$scope', 'monthReportService','calculateService',
+  function ($scope, monthReportService,calculateService) {
+    $scope.monthReportInfo = monthReportService.query({}, function () {
 
-          //计算积分增减
-          $scope.balance= function (pointAdd,pointReduce) {
-            return $scope.sub(pointAdd,pointReduce)
-          };
+      //总月报
+      $scope.monthReportDataT=$scope.monthReportInfo.data.total;
+      //月报list
+      $scope.monthReportDataL=$scope.monthReportInfo.data.detailList;
 
-          //计算充值还款
-          $scope.accumulate= function (pointAdd,pointReduce,recharge,withdraw) {
-            var point1=$scope.sub(pointAdd,pointReduce);
-            var point2=$scope.sub(recharge,withdraw);
-            return $scope.add(point1,point2)
-          }
-        });
+      $scope.monthReportDataTotal={
+        pointAdd:$scope.monthReportDataT.pointAdd||0.00,
+        pointReduce:$scope.monthReportDataT.pointReduce||0.00,
+        recharge:$scope.monthReportDataT.recharge||0.00,
+        withdraw:$scope.monthReportDataT.withdraw||0.00,
+        pointBalance:function () {
+          return calculateService.sub(this.pointAdd,this.pointReduce);
+        },
+        pointTotal: function () {
+          var point1=this.pointBalance();
+          var point2=calculateService.sub(this.recharge,this.withdraw);
+          return calculateService.add(point1,point2)
+        }
+      };
 
-        //精确计算加法
-        $scope.add= function (arg1,arg2) {
-          var arg1=Number(arg1);
-          var arg2=Number(arg2);
-          var r1,r2,m;
-          try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0}
-          try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0}
-          m=Math.pow(10,Math.max(r1,r2));
-          return (arg1*m+arg2*m)/m;
-        };
-        //精确计算减法
-        $scope.sub= function (arg1,arg2) {
-          var arg1=Number(arg1);
-          var arg2=Number(arg2);
-          var r1,r2,m,n;
-          try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0}
-          try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0}
-          m=Math.pow(10,Math.max(r1,r2));
-          //动态控制精度长度
-          n=(r1>=r2)?r1:r2;
-          return ((arg1*m-arg2*m)/m).toFixed(n);
-        };
+      $scope.monthReportDataL.pointBalance= function (index) {
+        return calculateService.sub($scope.monthReportDataL[index].pointAdd,$scope.monthReportDataL[index].pointReduce);
+      };
+      $scope.monthReportDataL.pointTotal= function (index) {
+        var point1=$scope.monthReportDataL.pointBalance(index);
+        var point2=calculateService.sub($scope.monthReportDataL[index].recharge,$scope.monthReportDataL[index].withdraw);
+        return calculateService.add(point1,point2)
+      }
 
+    });
 
-
-
-
-    }
+  }
 ]);
-
 
 
 //充值还款记录
 storeApp.register.controller('repayment', ['$scope',
-    function($scope) {
+  function($scope) {
 
-    }
+  }
 ])
 
 //提现记录
 storeApp.register.controller('cashrecord', ['$scope',
-    function($scope) {
+  function($scope) {
 
-    }
+  }
 ])
 
 //月报明细
-storeApp.register.controller('monthreportdetails', ['$scope', '$routeParams',
-    function($scope, $routeParams) {
+storeApp.register.controller('monthreportdetails', ['$scope', '$routeParams', 'monthDetailService',
+  function ($scope, $routeParams, monthDetailService) {
+    $scope.month = $routeParams.period.replace("-", "年");
 
-    }
-])
+    //$scope.monthDetailInfo = monthDetailService.query();
+
+    //按月份查找
+    $scope.monthDetailInfo=monthDetailService.get(
+        {
+          merchandId:storeApp.userInfo.merchantID,
+          period: $routeParams.period
+        },
+        function () {
+          //success
+          $scope.pointData=$scope.monthDetailInfo.data;
+
+          $scope.pointAddTotal=$scope.pointData.pointAddTotal<=0;
+          $scope.pointReduceTotal=$scope.pointData.pointReduceTotal<=0;
+          $scope.rwList=$scope.pointData.rwList.length<=0;
+        },
+        function () {
+          //error
+        }
+    );
+  }
+]);
--- modules/point/js/services.js
+++ modules/point/js/services.js
@@ -2,49 +2,139 @@
 
 //账户总览
 storeApp.register.factory('accountService', ['$resource',
-    function($resource) {
-        return $resource(globalConfig.api + 'point/pf/v1/accounts/:merchantId/points', { merchantId: "@merchantId" }, {
-            query: {
-                method: 'GET',
-                params: {
-                    offset: "@offset",
-                    limit: "@limit"
-                },
-                isArray: false
-            }
-        });
-    }
+  function($resource) {
+    return $resource(globalConfig.api + 'point/pf/v1/accounts/:merchantId/points', { merchantId: "@merchantId" }, {
+      query: {
+        method: 'GET',
+        params: {
+          offset: "@offset",
+          limit: "@limit"
+        },
+        isArray: false
+      }
+    });
+  }
 ]);
 
 //积分提现
 storeApp.register.factory('drawcashService', ['$resource',
-    function($resource) {
-        return $resource(globalConfig.api + 'point/pf/v1/withdraws', {}, {
-            save: {
-                method: 'POST',
-                params: {
-                    merchantId: "@merchantId",
-                    point: "@point",
-                    operaterId: "@operaterId"
-                },
-                isArray: false
-            }
-        });
-    }
+  function($resource) {
+    return $resource(globalConfig.api + 'point/pf/v1/withdraws', {}, {
+      save: {
+        method: 'POST',
+        params: {
+          merchantId: "@merchantId",
+          point: "@point",
+          operaterId: "@operaterId"
+        },
+        isArray: false
+      }
+    });
+  }
 ]);
+
+//积分交易明细
+storeApp.register.factory('transactionService',['$resource',
+    function ($resource) {
+      return $resource(
+          globalConfig.api+"point/pf/v1/accounts/:merchandId/points/details",
+          {
+            merchandId:storeApp.userInfo.merchantID,
+            startTime:"@startTime",
+            endTime:"@endTime"
+          },
+          {
+            query:{
+              method:"GET",
+              params: {},
+              isArray: false
+            }
+          }
+      )
+    }]
+);
 
 //积分月报
 storeApp.register.factory('monthReportService', ['$resource',
   function ($resource) {
     return $resource(
-          globalConfig.api + 'point/pf/v1/reports/:periodType/:showType', {
-          periodType: "month", showType: "m"
-        }, {
-          query: {
-            method: 'GET',
-            params: {},
-            isArray: false
+          globalConfig.api + 'point/pf/v1/reports/:periodType/:showType',
+          {
+            periodType: "month", showType: "pm"
+          },
+          {
+            query: {
+              method: 'GET',
+              params: {},
+              isArray: false
           }
         })
   }
 ]);
+
+//月报明细
+storeApp.register.factory('monthDetailService', ['$resource',
+  function ($resource) {
+    return $resource(
+        globalConfig.api + 'point/pf/v1/accounts/:merchandId/reports/month/details',
+        {
+
+        },
+        {
+        query: {
+          method: 'GET',
+          params: {
+
+          },
+          isArray: false
+        }
+    })
+  }
+]);
+
+//计算
+storeApp.register.factory('calculateService',[
+    function () {
+      return {
+        //精确计算减法
+        sub : function (arg1, arg2) {
+        var arg1 = Number(arg1);
+        var arg2 = Number(arg2);
+        var r1, r2, m, n;
+        try {
+          r1 = arg1.toString().split(".")[1].length
+        } catch (e) {
+          r1 = 0
+        }
+        try {
+          r2 = arg2.toString().split(".")[1].length
+        } catch (e) {
+          r2 = 0
+        }
+        m = Math.pow(10, Math.max(r1, r2));
+        //动态控制精度长度
+        n = (r1 >= r2) ? r1 : r2;
+        return ((arg1 * m - arg2 * m) / m).toFixed(n);
+      },
+        //精确计算加法
+        add : function (arg1, arg2) {
+          var arg1 = Number(arg1);
+          var arg2 = Number(arg2);
+          var r1, r2, m;
+          try {
+            r1 = arg1.toString().split(".")[1].length
+          } catch (e) {
+            r1 = 0
+          }
+          try {
+            r2 = arg2.toString().split(".")[1].length
+          } catch (e) {
+            r2 = 0
+          }
+          m = Math.pow(10, Math.max(r1, r2));
+          return (arg1 * m + arg2 * m) / m;
+        }
+      }
+
+
+}]);
